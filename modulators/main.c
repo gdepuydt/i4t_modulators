2e941f567589df362794fd8563c9b9a68876b176
Geert Depuydt <geert@custodix.com>
2018-12-04 15:33:00 +0100

    prepare VS project

--- modulators/main.c
+++ modulators/main.c
@@ -1,6 +1,21 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stddef.h>
+#include <assert.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <math.h>
+
+
 
 #include "../../i4t_lib/src/common.c"
+#include "modulators.c"
 
 void main() {
 	printf("Hello Modulators!\n");
